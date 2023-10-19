3ae8ecaa5672a806f593ebb7a16281b420af32e6
mhjoon99 <hyejoon8@gmail.com>
2023-10-19 16:51:46 +0900

    2070 큰놈,작은놈,같은놈

---
+++ SWEA/D1/2070_numComparison.py
@@ -0,0 +1,13 @@
+T = int(input())
+for test_case in range(1, T+1):
+    a, b = map(int, input().split())
+
+    if a > b:
+        result = '>'
+    elif a == b:
+        result = '='
+    else:
+        result = '<'
+
+    print("#"+str(test_case), end=" ")
+    print(result)
